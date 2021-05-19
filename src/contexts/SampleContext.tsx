fe9d1726e5cc30353560a4a41144269b0938a38e
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2021-05-20 00:33:15 +0300

    Adds sample context

---
+++ src/contexts/SampleContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+interface ISampleContext {}
+
+const SampleContext = createContext<ISampleContext>({});
+
+export const SampleProvider: React.FC = ({ children }) => {
+  return <SampleContext.Provider value={{}}>{children}</SampleContext.Provider>;
+};
